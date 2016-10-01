36bea28835d163f53d822e97d7a4e6c842a4b305
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-10-01 13:37:59 +1000

    Added player count

--- lib/wurm_rest_client/player.ex
+++ lib/wurm_rest_client/player.ex
@@ -7,28 +7,54 @@ defmodule WurmRestClient.Player do
 
   @rest_url Application.get_env(:wurm_rest_client, :rest_url)
 
-  @doc """
-  Given a player, fetches that player's current bank balance.
-  """
-  def fetch_money(player) do
-    player_money_url(player)
+  # Fetch a URL and handle the response
+  defp _fetch(url) do
+    url
     |> HTTPoison.get
     |> handle_response
   end
 
   @doc """
+  Fetch a count of all players currently playing
+  """
+  def fetch_count(), do: _fetch(players_count_url)
+
+  @doc """
+  Fetch all players
+  """
+  def fetch(), do: _fetch(players_url)
+
+  @doc """
+  Fetch the details for a given player
+  """
+  def fetch(player), do: _fetch(player_url(player))
+
+  @doc """
+  Given a player, fetches that player's current bank balance.
+  """
+  def fetch_money(player), do: _fetch(player_money_url(player))
+
+  @doc """
+  The URL for retrieving all players
+  """
+  def players_url(), do: "#{@rest_url}/players"
+
+  @doc """
+  The URL for retrieving a player count
+  """
+  def players_count_url, do: "#{players_url}?filter=total"
+
+  @doc """
   Given a player, returns the URL that corresponds to that player
   """
   def player_url(player) do
-    "#{@rest_url}/players/#{player.name}"
+    "#{players_url}/#{player.name}"
   end
 
   @doc """
   Given a player, returns the URL that corresponds to the location of the money resource for that player
   """
-  def player_money_url(player) do
-    "#{player_url(player)}/money"
-  end
+  def player_money_url(player), do: "#{player_url(player)}/money"
 
   @doc """
   Given a response to a Player request, returns a tuple depending on the response. If the API call was successfully
